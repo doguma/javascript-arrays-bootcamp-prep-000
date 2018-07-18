620f0b3766586c583c999885229a805f391a48a7
Hannah Do <hannah.hj.do@gmail.com>
2018-07-18 19:47:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,13 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element){
-  [element, ...array]
+  return [element, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.unshift(element);
+}
+
+function addElementToEndOfArray(array, element){
+  
 }
