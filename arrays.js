d37dccfc3d0dde2d1ef00a904c1a0cecb5ba3666
Hannah Do <hannah.hj.do@gmail.com>
2018-07-18 20:16:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  var tempArray = array.splice(0,element)
+  var tempArray = array.splice(0,1,element)
   return tempArray;
 }
 
